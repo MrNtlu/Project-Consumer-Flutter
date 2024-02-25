1959e57727d22ec9a858031bcfe3cfbf2b72270c
Burak Fidan <97burakfidan97@gmail.com>
2024-02-25 19:25:57 +0300

    Login header changed for iOS too.

--- lib/widgets/main/home/loggedin_header.dart
+++ lib/widgets/main/home/loggedin_header.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -21,118 +18,7 @@ class LoggedinHeader extends StatelessWidget {
     final image = authenticationProvider.basicUserInfo?.image;
     final isUrlValid = image != null && Uri.tryParse(image) != null;
 
-    return Platform.isIOS || Platform.isMacOS
-    ? CupertinoContextMenu(
-      enableHapticFeedback: true,
-      actions: [
-        CupertinoContextMenuAction(
-          onPressed: () {
-            Navigator.of(context, rootNavigator: true).push(
-              CupertinoPageRoute(builder: (_) {
-                return const ProfilePage();
-              })
-            );
-          },
-          trailingIcon: CupertinoIcons.profile_circled,
-          child: const Text('Profile'),
-        ),
-        CupertinoContextMenuAction(
-          onPressed: () {
-            Navigator.of(context, rootNavigator: true).push(
-              CupertinoPageRoute(builder: (_) {
-                return const CustomListPage();
-              })
-            );
-          },
-          trailingIcon: CupertinoIcons.folder_fill,
-          child: const Text('Custom Lists'),
-        ),
-        CupertinoContextMenuAction(
-          onPressed: () {
-            Navigator.of(context, rootNavigator: true).push(
-              CupertinoPageRoute(builder: (_) {
-                return const UserListPage();
-              })
-            );
-          },
-          trailingIcon: CupertinoIcons.list_bullet_below_rectangle,
-          child: const Text('User List'),
-        ),
-        CupertinoContextMenuAction(
-          onPressed: () {
-            Navigator.of(context, rootNavigator: true).push(
-              CupertinoPageRoute(builder: (_) {
-                return const ConsumeLaterPage();
-              })
-            );
-          },
-          trailingIcon: CupertinoIcons.time,
-          child: const Text('Watch Later'),
-        ),
-      ],
-      child: GestureDetector(
-        onTap: () {
-          Navigator.of(context, rootNavigator: true).push(
-            CupertinoPageRoute(builder: (_) {
-              return const ProfilePage();
-            })
-          );
-        },
-        child: ColoredBox(
-          color: CupertinoTheme.of(context).bgColor,
-          child: Padding(
-            padding: const EdgeInsets.all(8),
-            child: Row(
-              children: [
-                SizedBox(
-                  height: 32,
-                  width: 32,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: isUrlValid
-                    ? CachedNetworkImage(
-                      imageUrl: authenticationProvider.basicUserInfo!.image!,
-                      height: 30,
-                      width: 30,
-                      cacheKey: authenticationProvider.basicUserInfo!.image!,
-                      key: ValueKey<String>(authenticationProvider.basicUserInfo!.image!),
-                      fit: BoxFit.cover,
-                      progressIndicatorBuilder: (_, __, ___) =>
-                        const Padding(padding: EdgeInsets.all(3), child: CupertinoActivityIndicator()),
-                      errorListener: (_) {},
-                      errorWidget: (context, url, error) => const Icon(
-                        Icons.person,
-                        size: 30,
-                        color: CupertinoColors.activeBlue,
-                      ),
-                    )
-                    : const Icon(
-                      Icons.person,
-                      size: 30,
-                      color: CupertinoColors.activeBlue,
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: AutoSizeText(
-                    authenticationProvider.basicUserInfo?.username ?? '',
-                    minFontSize: 14,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    )
-    : GestureDetector(
+    return GestureDetector(
       onTap: () {
         Navigator.of(context, rootNavigator: true).push(
           CupertinoPageRoute(builder: (_) {
@@ -143,6 +29,8 @@ class LoggedinHeader extends StatelessWidget {
       child: Row(
         children: [
           PopupMenuButton(
+            icon: const Icon(CupertinoIcons.ellipsis_vertical),
+            iconSize: 26,
             color: CupertinoTheme.of(context).bgColor,
             itemBuilder: (context) => [
               PopupMenuItem(
